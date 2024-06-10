0c28d2e0f3e449405a76f3cacdaf94282cf2f011
adrianjsg <adrianjsg87@gmail.com>
2024-06-10 16:41:24 +0200

    spring boot

---
+++ src/test/java/com/sobrevela/proyectopersonal/ProyectopersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobrevela.proyectopersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
